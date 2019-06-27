60674d0b4a5a93611d259494074cc93d3e857b58
Tingting <Tingting@example.com>
2019-06-27 10:05:39 +0800

    Started project.

---
+++ alien_invasion.py
@@ -0,0 +1,29 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    ai_settings.screen_width = screen.get_rect().width
+    ai_settings.screen_height = screen.get_rect().height
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = pygame.sprite.Group()
+
+    # 开始游戏的主循环
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game()
